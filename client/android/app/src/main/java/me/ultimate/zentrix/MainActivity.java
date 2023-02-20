1b6e502d1c03685c3af52c0d3fa0f3c976c128ac
UltimateGG <clubblock1@gmail.com>
2023-02-19 18:18:25 -0600

    Android logos v2... okay??

--- client/android/app/src/main/java/me/ultimate/zentrix/MainActivity.java
+++ client/android/app/src/main/java/me/ultimate/zentrix/MainActivity.java
@@ -1,6 +1,7 @@
 package me.ultimate.zentrix;
 
 import android.os.Bundle;
+import android.view.Window;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
@@ -10,9 +11,11 @@ import com.getcapacitor.BridgeActivity;
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
 
         this.registerPlugin(GoogleAuth.class);
+
+        super.onCreate(savedInstanceState);
     }
 
     @Override
